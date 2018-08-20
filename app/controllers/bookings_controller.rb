a0aaf9bd8d5124a5d0b13bff2fc8c07b25408667
Thomas Colombet <tom1.webdev@gmail.com>
2018-08-20 18:45:07 +0200

    added method index & show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,13 @@
 class BookingsController < ApplicationController
 
+  def index
+    @booking = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
