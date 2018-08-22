403746be1af246fcee4f0332a4d7e1a0cd6a46e6
Thomas Colombet <tom1.webdev@gmail.com>
2018-08-22 12:08:34 +0200

    seed in work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Drone.delete_all
+User.delete_all
+
+users = ["Nay", "Andy", "Matho", "Tom", "Cartos"]
+drones = [""]
+
+users.each do |user|
+
+  mail = "#{user}@dronair.io"
+
+  user = User.create(email: mail, user_name: user, password: "DronAir2018")
+    rand(1..3).times do |y|
+      drone = Drone.new(
+        name: "drone - #{y}",
+        brand: "brand - #{y}",
+        autonomy: "autonomy - #{y}000",
+        price: "#{y}00",
+        video: "#{y}K"
+      )
+      drone.owner = user
+      drone.save
+    end
+end
