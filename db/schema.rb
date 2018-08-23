4044d3ee2379d4d5f28e71dd51f7d5e7a1476beb
Thomas Favre <thomasfavre01@gmail.com>
2018-08-23 11:56:01 +0200

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_143211) do
+ActiveRecord::Schema.define(version: 2018_08_22_141444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 2018_08_22_143211) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "available", default: true
     t.string "photo"
+    t.boolean "available"
     t.index ["user_id"], name: "index_drones_on_user_id"
   end
 
